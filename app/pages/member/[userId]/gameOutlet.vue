6d138766ceec601fcf360a11a9b942003c0e911b
tukazi-0227 <24wm0227@student.gs.chiba-u.jp>
2025-03-31 11:20:42 +0900

    ルーレットお店選択時のUI調整, セキュリティルールに伴うコードの変更

--- app/pages/member/[userId]/gameOutlet.vue
+++ app/pages/member/[userId]/gameOutlet.vue
@@ -27,8 +27,6 @@ const checkNumber = () => {
 };
 
 const selectedId = ref<string>("");
-
-
 const outletsData = ref<Outlet[]>([]);
 const selectedOutlets = ref<Outlet[]>([]);
 
@@ -101,7 +99,6 @@ onMounted(async () => {
       <div class="p-4 text-2xl font-semibold">
         <p>お店が全てそろいました</p>
         <p>ルーレットを開始してください</p>
-        <p>お店は以下です</p>
       </div>
       <div v-for="item in selectedOutlets" :key="item.id">
         <div class="flex items-center m-2">
@@ -127,48 +124,55 @@ onMounted(async () => {
     <div v-if="isModalOpen" class="relative">
       <div class="fixed inset-0 bg-gray-500/75"></div>
       <div class="fixed inset-0 flex items-center justify-center">
-        <div class="min-w-96 max-h-screen overflow-y-auto rounded-lg border-2 border-gray-400 bg-white">
-          <!-- お店選択画面 -->
-          <div v-if="isSelecting">
-            <p class="font-bold text-center p-3">お店を選んでください</p>
-            <div v-for="item in outletsData" :key="item.id">
-              <div class="flex items-center mx-2 my-2">
-                <input v-model="selectedId" type="radio" name="outlet" :value="item.id"
-                  class="size-6 accent-pink-500 mx-3" />
-                <div class="w-full flex items-center border-2 border-gray-300 rounded-lg"
-                  :class="{ 'border-2 border-pink-500 rounded-lg': selectedId === item.id }">
-                  <div class="w-[30%]">
-                    <img v-if="item.imageUrl" :src="item.imageUrl" alt="プレビュー画像" class="size-16 object-cover" />
-                    <img v-else-if="item.imageUrl" :src="item.imageUrl" alt="登録済画像" class="size-16 object-cover" />
-                    <div v-else
-                      class="flex size-16 items-center justify-center text-center rounded-lg bg-gray-400 text-white">
-                      No image
+        <div class="min-w-96 max-h-screen">
+          <div class="bg-white m-4 rounded-xl">
+            <!-- お店選択画面 -->
+            <div v-if="isSelecting">
+              <p class="font-bold text-center p-3">お店を選んでください</p>
+              <div class="h-96 overflow-y-auto">
+                <div v-for="item in outletsData" :key="item.id">
+                  <div class="flex items-center mx-2 my-2">
+                    <input v-model="selectedId" type="radio" name="outlet" :value="item.id"
+                      class="size-6 accent-pink-500 mx-3" />
+                    <div 
+                      class="w-full flex items-center border-2 border-gray-300 rounded-lg"
+                      :class="{ 'border-2 border-pink-500 rounded-lg': selectedId === item.id }"
+                      @click="selectedId = item.id"
+                    >
+                      <div class="w-[30%]">
+                        <img v-if="item.imageUrl" :src="item.imageUrl" alt="プレビュー画像" class="size-16 object-cover" />
+                        <img v-else-if="item.imageUrl" :src="item.imageUrl" alt="登録済画像" class="size-16 object-cover" />
+                        <div v-else
+                          class="flex size-16 items-center justify-center text-center rounded-lg bg-gray-400 text-white">
+                          No image
+                        </div>
+                      </div>
+                      <p class="text-center">{{ item.name }}</p>
                     </div>
                   </div>
-                  <p class="text-center">{{ item.name }}</p>
                 </div>
               </div>
-            </div>
 
-            <!-- ボタン -->
-            <div class="flex items-center justify-center">
-              <button class="bg-black text-white font-semibold px-4 py-1 rounded-2xl m-2"
-                @click="pushOutlet(selectedId)">選択する</button>
+              <!-- ボタン -->
+              <div class="flex items-center justify-center">
+                <button class="bg-black text-white font-semibold px-4 py-1 rounded-2xl m-2"
+                  @click="pushOutlet(selectedId)">選択する</button>
+              </div>
             </div>
-          </div>
-
-          <!-- 次の人に渡す画面 or 完了画面でルーレット回す画面 -->
-          <div v-else>
-            <p class="font-bold text-center p-3">お店選択が完了しました！！</p>
-            <p class="font-semibold p-3">次の人に渡してください</p>
-            <div class="flex items-center justify-center">
-              <div class="size-16 text-center text-5xl font-bold">{{ selectedOutlets.length + 1 }}</div>
-              <p class="text-end">人目</p>
-            </div>
-            <!-- ボタン -->
-            <div class="flex items-center justify-center">
-              <button class="bg-black text-white font-semibold px-4 py-1 rounded-2xl m-2"
-                @click="checkAddCount">次の人へ</button>
+  
+            <!-- 次の人に渡す画面 or 完了画面でルーレット回す画面 -->
+            <div v-else>
+              <p class="font-bold text-center p-3">お店選択が完了しました！！</p>
+              <p class="font-semibold p-3">次の人に渡してください</p>
+              <div class="flex items-center justify-center">
+                <div class="size-16 text-center text-5xl font-bold">{{ selectedOutlets.length + 1 }}</div>
+                <p class="text-end">人目</p>
+              </div>
+              <!-- ボタン -->
+              <div class="flex items-center justify-center">
+                <button class="bg-black text-white font-semibold px-4 py-1 rounded-2xl m-2"
+                  @click="checkAddCount">次の人へ</button>
+              </div>
             </div>
           </div>
         </div>
